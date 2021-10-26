42e5954ac5292c98a60cae99be83ad89da64c730
Dibin Nadar <nadar.dibin@gmail.com>
2021-10-26 15:03:22 +0530

    Relation Mechanisim before overhaul

--- src/main/java/com/example/geektrust/Dynasty.java
+++ src/main/java/com/example/geektrust/Dynasty.java
@@ -2,7 +2,6 @@ package com.example.geektrust;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Dynasty {
 
@@ -11,20 +10,20 @@ public class Dynasty {
     private final Node queenNode;
     private final Node kingNode;
 
-    public Dynasty(String queenName, String kingName,String dynastyName) {
+    public Dynasty(String queenName, String kingName, String dynastyName) {
         this.familyTree = new HashMap<>();
-        this.queenNode = new Node(queenName,Gender.FEMALE);
-        this.kingNode = new Node(kingName,Gender.MALE);
-        this.dynastyName= dynastyName;
+        this.queenNode = new Node(queenName, Gender.FEMALE);
+        this.kingNode = new Node(kingName, Gender.MALE);
+        this.dynastyName = dynastyName;
 
-        this.familyTree.put(queenNode.getName(),queenNode);
-        this.familyTree.put(kingNode.getName(),kingNode);
+        this.familyTree.put(queenNode.getName(), queenNode);
+        this.familyTree.put(kingNode.getName(), kingNode);
 
         queenNode.addPartnerNameOnlyOnce(kingNode.getName());
         kingNode.addPartnerNameOnlyOnce(queenNode.getName());
     }
 
-    public boolean addKid(String motherName, String kidName, Gender kidGender){
+    public boolean addKid(String motherName, String kidName, Gender kidGender) {
 
         // Mother not in dynasty
         if (!this.familyTree.containsKey(motherName)) return false;
@@ -35,13 +34,13 @@ public class Dynasty {
         Node mother = this.familyTree.get(motherName);
 
         // Mother not Female
-        if (mother.getGender()!=Gender.FEMALE) return false;
+        if (mother.getGender() != Gender.FEMALE) return false;
 
         // Mother has no partner
-        if (mother.getPartnerName()==null) return false;
+        if (mother.getPartnerName() == null) return false;
 
-        Node kid = new Node(kidName,kidGender);
-        this.familyTree.put(kidName,kid);
+        Node kid = new Node(kidName, kidGender);
+        this.familyTree.put(kidName, kid);
 
         kid.addMotherOnlyOnce(motherName);
         mother.addKidToMother(kidName);
@@ -58,43 +57,39 @@ public class Dynasty {
 
         // Identify the new individual
 
-        if (!this.familyTree.containsKey(femaleName)){
+        if (!this.familyTree.containsKey(femaleName)) {
             male = this.familyTree.get(maleName);
-            female = new Node(femaleName,Gender.FEMALE);
-        }else {
+            female = new Node(femaleName, Gender.FEMALE);
+        } else {
             female = this.familyTree.get(femaleName);
-            male = new Node(maleName,Gender.MALE);
+            male = new Node(maleName, Gender.MALE);
         }
 
         // Someone already married
-        if (female.getPartnerName()!=null || male.getPartnerName()!=null) return false;
+        if (female.getPartnerName() != null || male.getPartnerName() != null) return false;
 
         // Incorrect genders passed
-        if (female.getGender()!=Gender.FEMALE || male.getGender()!=Gender.MALE) return false;
+        if (female.getGender() != Gender.FEMALE || male.getGender() != Gender.MALE) return false;
 
         // add new individual to the dynasty
-        this.familyTree.put(femaleName,female);
-        this.familyTree.put(maleName,male);
+        this.familyTree.put(femaleName, female);
+        this.familyTree.put(maleName, male);
 
         return male.addPartnerNameOnlyOnce(femaleName) && female.addPartnerNameOnlyOnce(maleName);
     }
 
-    public String getRelationship(String relationOf, Relation relationship){ // TODO better names
+    public String getRelationship(String relationOf, Relation relationship) { // TODO better names
 
         String defaultResponse = "PERSON_NOT_FOUND";
         String relation;
 
         if (!containsOne(relationOf)) return defaultResponse;
 
-        switch (relationship){
-// todo return multiple
-
+        switch (relationship) {
 
             case MOTHER:
                 relation = getOne(relationOf).getMotherName();
                 return relation == null ? defaultResponse : relation;
-
-            //      this.partnerName = this.partnerName == null ? partnerName : this.partnerName;
 
             case FATHER:
                 String mother = getRelationship(relationOf, Relation.MOTHER);
@@ -103,97 +98,30 @@ public class Dynasty {
                 relation = getOne(mother).getPartnerName();
                 return relation == null ? defaultResponse : relation;
 
-
             case CHILDREN:
-                if (getOne(relationOf).getPartnerName() == null) return defaultResponse;
-
-                if (getOne(relationOf).getGender().equals(Gender.MALE)){
-                    mother = getOne(relationOf).getPartnerName();
-                }else mother = getOne(relationOf).getName();
-
-                Set<String> allKids = new HashSet<>(getOne(mother).getKids());
-//                System.out.println(allKids.toString());
-                return  allKids.isEmpty() ? defaultResponse : allKids.toString();
-
+                return findAllChildren(relationOf, defaultResponse);
 
             case DAUGHTER:
-////                if (getOne(relationOf).getPartnerName() == null) return defaultResponse;
-////
-////                if (getOne(relationOf).getGender().equals(Gender.MALE)){
-////                    mother = getOne(relationOf).getPartnerName();
-////                }else mother = getOne(relationOf).getName();
-////
-////                allKids = new HashSet<>(getOne(mother).getKids());
-////                if (allKids.isEmpty()) return defaultResponse;
-//
-////                allKids = getRelationship(relationOf,Relation.CHILDREN)
-//                String kidlings = getRelationship(relationOf,Relation.CHILDREN);
-//                kidlings = kidlings.substring(1,kidlings.length()-1);
-//                System.out.println("dsdsdssdd");
-//                System.out.println(kidlings);
-////                System.out.println((Arrays.toString(kidlings.split(", "))));
-//                String[] ary = kidlings.split(", ");
-//
-//                List<String> aaa = Arrays
-//                        .stream(ary)
-//                        .filter(kid -> getOne(kid)
-//                                .getGender()
-//                                .equals(Gender.FEMALE))
-//                        .collect(Collectors.toList());
-////                        .collect(Collectors.toSet());
-//
-////                return aaa.toString();
-//                            return  aaa.isEmpty() ? defaultResponse : aaa.toString();
-//
-////                System.out.println(ary.length);
-////
-////                for (String s: ary){
-////                    System.out.println(s);
-////                }
-//
-////                Set<String> theDaughters = Stream.of(kidlings.split(", "))
-////                        .filter(kid -> getOne(kid)
-////                                .getGender()
-////                                .equals(Gender.FEMALE))
-////                        .collect(Collectors.toSet());
-//
-////                String[] daughters = kidlings.stream()
-////                        .filter(kid -> getOne(kid).getGender().equals(Gender.FEMALE))
-////                        .collect(Collectors.toSet());
-////
-////                Set<String> daughters = allKids.stream()
-////                        .filter(kid -> getOne(kid).getGender().equals(Gender.FEMALE))
-////                        .collect(Collectors.toSet());
-//
-////                return  theDaughters.isEmpty() ? defaultResponse : theDaughters.toString();
-////                return  daughters.isEmpty() ? defaultResponse : daughters.toString();
-
-
+                return findChildrenByGender(relationOf, defaultResponse, Gender.FEMALE);
 
             case SON:
-//                if (getOne(relationOf).getPartnerName() == null) return defaultResponse;
-//
-//                if (getOne(relationOf).getGender().equals(Gender.MALE)){
-//                    mother = getOne(relationOf).getPartnerName();
-//                }else mother = getOne(relationOf).getName();
-//
-//                allKids = new HashSet<>(getOne(mother).getKids());
-//                if (allKids.isEmpty()) return defaultResponse;
-//
-//
-//                Set<String> sons = allKids.stream()
-//                        .filter(kid -> getOne(kid).getGender().equals(Gender.MALE))
-//                        .collect(Collectors.toSet());
-//
-//                return  sons.isEmpty() ? defaultResponse : sons.toString();
+                return findChildrenByGender(relationOf, defaultResponse, Gender.MALE);
 
+            case SIBLINGS:
+                return findAllSiblings(relationOf, defaultResponse);
 
             case SISTER:
+                return findSiblingsByGender(relationOf, defaultResponse, Gender.FEMALE);
+
             case BROTHER:
-            case SIBLINGS:
+                return findSiblingsByGender(relationOf, defaultResponse, Gender.MALE);
+
 
             case SISTER_IN_LAW:
+                return findSisterInLaw(relationOf,defaultResponse);
+
             case BROTHER_IN_LAW:
+                return findBrotherInLaw(relationOf,defaultResponse);
 
             case MATERNAL_AUNT:
             case MATERNAL_UNCLE:
@@ -202,20 +130,159 @@ public class Dynasty {
             case PATERNAL_UNCLE:
 
 
+            default:
+                return defaultResponse;
+        }
+    }
+
+    // TODO Unify all methods structure, return same dataTypes, basically be more elegant
+    //  code consistency, stringBuilder everywhere?
 
 
-            default: return defaultResponse;
+    private String findAllChildren(String relationOf, String defaultResponse) {
+        if (getOne(relationOf).getPartnerName() == null) return defaultResponse;
 
+        String mother;
+        if (getOne(relationOf).getGender().equals(Gender.MALE)) {
+            mother = getOne(relationOf).getPartnerName();
+        } else mother = getOne(relationOf).getName();
+
+        Set<String> allKids = new HashSet<>(getOne(mother).getKids());
+        List<String> all = new LinkedList<>(getOne(mother).getKids());
+        return all.isEmpty() ? defaultResponse : all.toString();
+//        return allKids.isEmpty() ? defaultResponse : allKids.toString();
+    }
+
+    private String findChildrenByGender(String relationOf, String defaultResponse, Gender gender) {
+
+        String kids = findAllChildren(relationOf, defaultResponse);
+        if (kids.equals(defaultResponse)) return defaultResponse;
+
+        kids = kids.substring(1, kids.length() - 1); // removing []
+        String[] kidsArray = kids.split(", ");
+
+        List<String> filteredKidList = Arrays
+                .stream(kidsArray)
+                .filter(kid -> getOne(kid)
+                        .getGender()
+                        .equals(gender))
+                .collect(Collectors.toList());
+
+        return filteredKidList.isEmpty() ? defaultResponse : filteredKidList.toString();
+
+    }
+
+    // TODO: Check if too much code hopping
+    private String findAllSiblings(String relationOf, String defaultResponse) {
+
+        String mother = getRelationship(relationOf, Relation.MOTHER);
+        if (mother.equals(defaultResponse)) return defaultResponse;
+
+        Set<String> allKids = new HashSet<>(getOne(mother).getKids());
+
+        List<String> allSiblings = allKids.stream().filter(kid -> !kid.equals(relationOf)).collect(Collectors.toList());
+
+        return allSiblings.isEmpty() ? defaultResponse : allSiblings.toString();
+    }
+
+    private String findSiblingsByGender(String relationOf, String defaultResponse, Gender gender){
+
+        String siblings = findAllSiblings(relationOf, defaultResponse);
+        if (siblings.equals(defaultResponse)) return defaultResponse;
+
+        siblings = siblings.substring(1, siblings.length() - 1);
+        String[] kidsArray = siblings.split(", ");
+
+        List<String> filteredKidList = Arrays
+                .stream(kidsArray)
+                .filter(kid -> getOne(kid)
+                        .getGender()
+                        .equals(gender))
+                .collect(Collectors.toList());
+
+        return filteredKidList.isEmpty() ? defaultResponse : filteredKidList.toString();
+
+    }
+
+    // TODO needs major attention
+    private String findBrotherInLaw(String relationOf, String defaultResponse){
+        String partner = getOne(relationOf).getPartnerName();
+        String mother = getOne(relationOf).getMotherName();
+
+        if (mother != null) { // belongs to bloodline
+            String sisters = findSiblingsByGender(relationOf,defaultResponse,Gender.FEMALE);
+            if (sisters.equals(defaultResponse)) return defaultResponse;
+
+            StringBuilder brotherInLaw = new StringBuilder();
+
+            sisters = sisters.substring(1, sisters.length() - 1);
+            String[] sistersArray = sisters.split(", ");
+
+            List<String> sisterList = Arrays
+                    .stream(sistersArray)
+                    .filter(sister -> getOne(sister)
+                            .getGender()
+                            .equals(Gender.FEMALE))
+                    .collect(Collectors.toList());
+
+            for (String sister:sisterList){
+                String husband = getOne(sister).getPartnerName();
+                if (!husband.equals(defaultResponse)){
+                    brotherInLaw.append(husband).append(" ");  // TODO check formaot in sisterInLaw for Atya
+                }
+            }
+
+            return brotherInLaw.toString();
+        }
+
+
+        String filteredBrotherInLaw = findSiblingsByGender(partner,defaultResponse,Gender.MALE);
+
+        return filteredBrotherInLaw.isEmpty() ? defaultResponse : filteredBrotherInLaw;
+    }
+
+    // TODO needs major attention
+    private String findSisterInLaw(String relationOf, String defaultResponse){
+        String partner = getOne(relationOf).getPartnerName();
+        String mother = getOne(relationOf).getMotherName();
+
+        if (mother != null) { // belongs to bloodline
+            String brothers = findSiblingsByGender(relationOf,defaultResponse,Gender.MALE);
+            if (brothers.equals(defaultResponse)) return defaultResponse;
+
+            StringBuilder sisterInLaw = new StringBuilder();
+
+            brothers = brothers.substring(1, brothers.length() - 1);
+            String[] brothersArray = brothers.split(", ");
+
+            List<String> brotherList = Arrays
+                    .stream(brothersArray)
+                    .filter(brother -> getOne(brother)
+                            .getGender()
+                            .equals(Gender.MALE))
+                    .collect(Collectors.toList());
+
+            for (String brother:brotherList){
+                String wife = getOne(brother).getPartnerName();
+                if (wife != null){
+                    sisterInLaw.append(wife).append(" "); // TODO check formaot in sisterInLaw for Atya
+                }
+            }
+            return sisterInLaw.toString().isEmpty() ? defaultResponse : sisterInLaw.toString();
 
         }
 
+
+        String filteredSisterInLaw = findSiblingsByGender(partner,defaultResponse,Gender.FEMALE);
+
+        return filteredSisterInLaw.isEmpty() ? defaultResponse : filteredSisterInLaw;
     }
 
-    public Node getOne(String nodeName){
+    public Node getOne(String nodeName) {
         return familyTree.get(nodeName);
     }
 
-    public boolean containsOne(String nodeName){
+    public boolean containsOne(String nodeName) {
         return familyTree.containsKey(nodeName);
     }
 
--- src/main/java/com/example/geektrust/Main.java
+++ src/main/java/com/example/geektrust/Main.java
@@ -112,27 +112,71 @@ public class Main {
         System.out.println("");
 
         System.out.println("----------------Daughter-------------------");
-//        System.out.println(lengaburuFamilyTree.getRelationship("Anga",Relation.DAUGHTER));  //  satya
-//        System.out.println(lengaburuFamilyTree.getRelationship("Dritha",Relation.DAUGHTER));
-//        System.out.println(lengaburuFamilyTree.getRelationship("Jnki",Relation.DAUGHTER));  // lavnya
-//        System.out.println(lengaburuFamilyTree.getRelationship("Arit",Relation.DAUGHTER));  // lavnya
-//        System.out.println(lengaburuFamilyTree.getRelationship("Vritha",Relation.DAUGHTER));
-//        System.out.println(lengaburuFamilyTree.getRelationship("Amba",Relation.DAUGHTER));  // dritha tritha
-//        System.out.println(lengaburuFamilyTree.getRelationship("Drithaaa",Relation.DAUGHTER));
+        System.out.println(lengaburuFamilyTree.getRelationship("Anga",Relation.DAUGHTER));  //  satya
+        System.out.println(lengaburuFamilyTree.getRelationship("Dritha",Relation.DAUGHTER));
+        System.out.println(lengaburuFamilyTree.getRelationship("Jnki",Relation.DAUGHTER));  // lavnya
+        System.out.println(lengaburuFamilyTree.getRelationship("Arit",Relation.DAUGHTER));  // lavnya
+        System.out.println(lengaburuFamilyTree.getRelationship("Vritha",Relation.DAUGHTER));
+        System.out.println(lengaburuFamilyTree.getRelationship("Amba",Relation.DAUGHTER));  // dritha tritha
+        System.out.println(lengaburuFamilyTree.getRelationship("Drithaaa",Relation.DAUGHTER));
         System.out.println("");
-//
-//        System.out.println("----------------Son-------------------");
-//        System.out.println(lengaburuFamilyTree.getRelationship("Anga",Relation.SON));  //  chit, ish, vich, aras
-//        System.out.println(lengaburuFamilyTree.getRelationship("Vich",Relation.SON));
-//        System.out.println(lengaburuFamilyTree.getRelationship("Jnki",Relation.SON));  // laki
-//        System.out.println(lengaburuFamilyTree.getRelationship("Arit",Relation.SON));  // laki
-//        System.out.println(lengaburuFamilyTree.getRelationship("Vritha",Relation.SON));
-//        System.out.println(lengaburuFamilyTree.getRelationship("Drithaaa",Relation.SON));
+
+        System.out.println("----------------Son-------------------");
+        System.out.println(lengaburuFamilyTree.getRelationship("Anga",Relation.SON));  //  chit, ish, vich, aras
+        System.out.println(lengaburuFamilyTree.getRelationship("Vich",Relation.SON));
+        System.out.println(lengaburuFamilyTree.getRelationship("Jnki",Relation.SON));  // laki
+        System.out.println(lengaburuFamilyTree.getRelationship("Arit",Relation.SON));  // laki
+        System.out.println(lengaburuFamilyTree.getRelationship("Vritha",Relation.SON));
+        System.out.println(lengaburuFamilyTree.getRelationship("Drithaaa",Relation.SON));
+        System.out.println("");
+
+        System.out.println("----------------Siblings-------------------");
+        System.out.println(lengaburuFamilyTree.getRelationship("Anga",Relation.SIBLINGS));
+        System.out.println(lengaburuFamilyTree.getRelationship("Vich",Relation.SIBLINGS));  // chit, ish, aras, satya
+        System.out.println(lengaburuFamilyTree.getRelationship("Jnki",Relation.SIBLINGS));  // ahit
+        System.out.println(lengaburuFamilyTree.getRelationship("Arit",Relation.SIBLINGS));  
+        System.out.println(lengaburuFamilyTree.getRelationship("Drithaaa",Relation.SIBLINGS));
+        System.out.println("");
+        
+        System.out.println("----------------Sister-------------------");
+        System.out.println(lengaburuFamilyTree.getRelationship("Anga",Relation.SISTER));
+        System.out.println(lengaburuFamilyTree.getRelationship("Vich",Relation.SISTER));  // satya
+        System.out.println(lengaburuFamilyTree.getRelationship("Jnki",Relation.SISTER));
+        System.out.println(lengaburuFamilyTree.getRelationship("Arit",Relation.SISTER));  
+        System.out.println(lengaburuFamilyTree.getRelationship("Drithaaa",Relation.SISTER));
+        System.out.println("");
+
+        System.out.println("----------------Brother-------------------");
+        System.out.println(lengaburuFamilyTree.getRelationship("Anga",Relation.BROTHER));
+        System.out.println(lengaburuFamilyTree.getRelationship("Vich",Relation.BROTHER));  // chit, ish, aras
+        System.out.println(lengaburuFamilyTree.getRelationship("Jnki",Relation.BROTHER));  // ahit
+        System.out.println(lengaburuFamilyTree.getRelationship("Arit",Relation.BROTHER));
+        System.out.println(lengaburuFamilyTree.getRelationship("Drithaaa",Relation.BROTHER));
+        System.out.println("");
+
+        System.out.println("----------------Brother_In_Law-------------------");
+        System.out.println(lengaburuFamilyTree.getRelationship("Anga",Relation.BROTHER_IN_LAW));
+        System.out.println(lengaburuFamilyTree.getRelationship("Lika",Relation.BROTHER_IN_LAW));  // chit, ish, aras
+        System.out.println(lengaburuFamilyTree.getRelationship("Jnki",Relation.BROTHER_IN_LAW));
+        System.out.println(lengaburuFamilyTree.getRelationship("Arit",Relation.BROTHER_IN_LAW));  // ahit
+        System.out.println(lengaburuFamilyTree.getRelationship("Ahit",Relation.BROTHER_IN_LAW));  // arit
+        System.out.println(lengaburuFamilyTree.getRelationship("Drithaaa",Relation.BROTHER_IN_LAW));
+        System.out.println(lengaburuFamilyTree.getRelationship("Vich",Relation.BROTHER_IN_LAW));  // vyan
+        System.out.println("");
+
+        System.out.println("----------------Sister_In_Law-------------------");
+        System.out.println(lengaburuFamilyTree.getRelationship("Anga",Relation.SISTER_IN_LAW));
+        System.out.println(lengaburuFamilyTree.getRelationship("Lika",Relation.SISTER_IN_LAW));  // satya
+        System.out.println(lengaburuFamilyTree.getRelationship("Jnki",Relation.SISTER_IN_LAW));
+        System.out.println(lengaburuFamilyTree.getRelationship("Arit",Relation.SISTER_IN_LAW));
+        System.out.println(lengaburuFamilyTree.getRelationship("Ahit",Relation.SISTER_IN_LAW));
+        System.out.println(lengaburuFamilyTree.getRelationship("Drithaaa",Relation.SISTER_IN_LAW));
+        System.out.println(lengaburuFamilyTree.getRelationship("Vich",Relation.SISTER_IN_LAW));  // amba chitra
+        System.out.println("");
+
+
 //        System.out.println("");
-
-
-        System.out.println("");
-                System.out.println(lengaburuFamilyTree.getRelationship("Jnki",Relation.DAUGHTER));  // lavnya
+//                System.out.println(lengaburuFamilyTree.getRelationship("Jnki",Relation.DAUGHTER));  // lavnya
 
     }
 }
