ef4a9ed869e2a1df1524da827356908c77437116
Dibin Nadar <nadar.dibin@gmail.com>
2021-11-04 23:56:33 +0530

    Removed redundant classes and files

--- src/main/java/com/example/geektrust/Main.java
+++ src/main/java/com/example/geektrust/Main.java
@@ -3,6 +3,7 @@ package com.example.geektrust;
 import java.io.InputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 public class Main {
 
@@ -13,20 +14,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        String inputString = Arrays.toString(args);
-//        inputString = inputString.substring(1, inputString.length() - 1);
-//        Path pathForDataQuery = FileSystems.getDefault().getPath(inputString);
-//        InputProcessor.modifyAndVerifyData(pathForDataQuery);
+        String inputString = Arrays.toString(args);
+        inputString = inputString.substring(1, inputString.length() - 1);
+        Path pathForDataQuery = FileSystems.getDefault().getPath(inputString);
+        InputProcessor.modifyAndVerifyData(pathForDataQuery);
 
-
-        String inputPathLocal = "G:\\GeekTrust\\MeetTheFamily\\java-maven-starter-kit\\sample_input\\queryTest.txt";
-        Path pathForDataQueryLocal = FileSystems.getDefault().getPath(inputPathLocal);
-        InputProcessor.modifyAndVerifyData(pathForDataQueryLocal);
-
-
-        // Easy cmds
-//        G:\GeekTrust\MeetTheFamily\java-maven-starter-kit>mvn package
-//        G:\GeekTrust\MeetTheFamily\java-maven-starter-kit\target>java -jar geektrust.jar G:\GeekTrust\MeetTheFamily\java-maven-starter-kit\sample_input\queryTest.txt
     }
 
     private static InputStream getFileFromResourceAsStream(String fileName) {
